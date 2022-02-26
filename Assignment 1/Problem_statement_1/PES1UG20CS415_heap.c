f599d174b58e4b61bc5602105a400968f2bcaaaa
shrujandev <shrujandev1824@gmail.com>
2022-02-26 22:34:17 +0530

    Init and Insert Functions created

--- Assignment 1/Problem_statement_1/PES1UG20CS415_heap.c
+++ Assignment 1/Problem_statement_1/PES1UG20CS415_heap.c
@@ -1,5 +1,5 @@
-// Name:
-// SRN: 
+// Name:Shrujan
+// SRN: PES1UG20CS415
 #include "heap.h"
 #include <stdlib.h>
 
@@ -17,12 +17,53 @@
 // Set heap size to 0
 // Set heap max_size to paramter max_size
 // Allocate memory of max_size to the array
-void init_heap(heap_t *heap, int max_size) {}
+void init_heap(heap_t *heap, int max_size) {
+    heap->arr = (int*)malloc(max_size*sizeof(int));
+    heap->max_size = max_size;
+    heap->size = 0;
+
+}
 
 // Insert element "key" to heap "heap"
 // and store the number of key comparisons made in the
 // location pointed to by count_ptr.
-void insert(heap_t *heap, int key, int *count_ptr) {}
+void insert(heap_t *heap, int key, int *count_ptr) {
+    if(heap->size < heap->max_size){
+        heap->size++;
+        heap->arr[heap->size] = key;
+        int heapifyLoop = 1;
+        for(int parentNode=(heap->size)/2-1;parentNode>=0;parentNode--){
+            while(heapifyLoop){
+                if(heap->size !=1){
+                    int largest = parentNode;
+                    int leftNode = 2*parentNode+1;
+                    int rightNode = 2*parentNode+2;
+                    if(leftNode < heap->size && heap->arr[leftNode] > heap->arr[largest]){
+                        largest = leftNode;
+                        *count_ptr++;
+                    }
+                    if(rightNode < heap->size && heap->arr[rightNode] > heap->arr[largest]){
+                        largest = rightNode;
+                        *count_ptr++;
+                    }
+                    if(largest != parentNode){
+                        int temp = heap->arr[largest];
+                        heap->arr[largest] = heap->arr[parentNode];
+                        heap->arr[parentNode] = heap->arr[largest];
+                        parentNode = largest ; 
+                    }
+                    else{
+                        heapifyLoop = 0;
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
+
+}
 
 // *Removes and Returns* the maximum element in the heap
 // and store the number of key comparisons made in the
