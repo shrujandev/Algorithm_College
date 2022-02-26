b2ae9c8ec4d241c4feeba296c24bee3efb921b6f
shrujandev <shrujandev1824@gmail.com>
2022-02-26 23:36:42 +0530

    Extract Function and search Function

--- Assignment 1/Problem_statement_1/PES1UG20CS415_heap.c
+++ Assignment 1/Problem_statement_1/PES1UG20CS415_heap.c
@@ -33,7 +33,51 @@ void insert(heap_t *heap, int key, int *count_ptr) {
         heap->arr[heap->size] = key;
         int heapifyLoop = 1;
         for(int parentNode=(heap->size)/2-1;parentNode>=0;parentNode--){
-            while(heapifyLoop){
+            heapify(heap,parentNode,heapifyLoop,count_ptr);
+        }
+        
+    }
+    
+
+}
+
+// *Removes and Returns* the maximum element in the heap
+// and store the number of key comparisons made in the
+// location pointed to by count_ptr.
+int extract_max(heap_t *heap, int *count_ptr) {
+    int maxValue = -1;
+    if(heap->size > 0){
+        maxValue = heap->arr[0];
+        heap->arr[0]=0;
+        heap->size--;
+        for(int parentNode=(heap->size)/2-1;parentNode>=0;parentNode--){
+            heapify(heap,parentNode,1,count_ptr);
+        }
+    }
+    return maxValue;
+    
+}
+
+// Searches for the element key in the heap
+// Returns the element if found, else -1
+int search(const heap_t *heap, int key, int *count_ptr) {}
+
+// Returns the maximum value of the element in the heap
+// and store the number of key comparisons made in the
+// location pointed to by count_ptr.
+int find_max(const heap_t *heap, int *count_ptr) {}
+
+// Returns the minimum value in the heap
+int find_min(const heap_t *heap, int *count_ptr) {}
+
+// Clears the heap for reuse
+void clear_heap(heap_t *heap) {}
+
+// Frees all resources acquired to initialize heap
+void free_heap(heap_t *heap) {}
+
+static void heapify(heap_t *heap,int parentNode,int heapifyLoop,int *count_ptr){
+    while(heapifyLoop){
                 if(heap->size !=1){
                     int largest = parentNode;
                     int leftNode = 2*parentNode+1;
@@ -58,32 +102,4 @@ void insert(heap_t *heap, int key, int *count_ptr) {
                     
                 }
             }
-        }
-        
-    }
-    
-
 }
-
-// *Removes and Returns* the maximum element in the heap
-// and store the number of key comparisons made in the
-// location pointed to by count_ptr.
-int extract_max(heap_t *heap, int *count_ptr) {}
-
-// Searches for the element key in the heap
-// Returns the element if found, else -1
-int search(const heap_t *heap, int key, int *count_ptr) {}
-
-// Returns the maximum value of the element in the heap
-// and store the number of key comparisons made in the
-// location pointed to by count_ptr.
-int find_max(const heap_t *heap, int *count_ptr) {}
-
-// Returns the minimum value in the heap
-int find_min(const heap_t *heap, int *count_ptr) {}
-
-// Clears the heap for reuse
-void clear_heap(heap_t *heap) {}
-
-// Frees all resources acquired to initialize heap
-void free_heap(heap_t *heap) {}
