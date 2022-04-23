eac4d2d5c4bccddf3a99a3524302b63d00453823
shrujandev <shrujandev1824@gmail.com>
2022-04-23 19:08:43 +0530

    Initial Commit

---
+++ Assignment 234-20220423T131850Z-001/Assignment 234/Source_code/PES1UG20CS000.c
@@ -0,0 +1,63 @@
+#include "header.h"
+
+// ANY STATIC FUNCTIONS ARE UP HERE
+
+
+// YOUR SOLUTIONS BELOW
+
+int q1(int n, const connection_t connections[n][n])
+{
+    return 0;
+}
+
+int q2(const airport_t *src, const airport_t *dest, int n, int k,
+       const connection_t connections[n][n])
+{
+    return 0;
+}
+
+int q3(const airport_t *src, int n, const connection_t connections[n][n])
+{
+    return 0;
+}
+
+void q4(int n, int (*predicate_func)(const airport_t *, const airport_t *),
+        airport_t airport_list[n])
+{
+
+}
+
+pair_t q5(int n, airport_t airports[n])
+{
+    pair_t ans = {-1, -1};
+    return ans;
+}
+
+int q6(int n, int amount, const int entry_fee[n])
+{
+    return 0;
+}
+
+void q7(int n, const char *pat, int contains[n], const airport_t airports[n])
+{
+
+}
+
+int q8(int n, int trip_order[n - 1], const connection_t connections[n][n])
+{
+    return 0;
+}
+
+int q9(int n, pair_t edges[n - 1], const connection_t connections[n][n])
+{
+    return 0;
+}
+
+void q10(int n, int k, const airport_t *src,
+         const connection_t connections[n][n], const int destinations[k],
+         int costs[k])
+{
+
+}
+
+// END
