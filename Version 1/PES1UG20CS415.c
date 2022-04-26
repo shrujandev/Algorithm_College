0b8db3cb6897efbccfe2bcb90abe198f2595b617
shrujandev <shrujandev1824@gmail.com>
2022-04-26 17:07:38 +0530

    Q1 Passed

---
+++ Version 1/PES1UG20CS415.c
@@ -0,0 +1,95 @@
+#include "header.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// ANY STATIC FUNCTIONS ARE UP HERE
+
+static void dfsQ1(int x,int *visited,int n,const connection_t graph[n][n]){
+    visited[x] = 1;
+    for(int i = x;i < n;i++){
+        for(int j = 0;j < n;j++){
+            if(graph[i][j].distance != __INT_MAX__ && visited[j] != 1){
+                dfsQ1(j, visited, n, graph);
+            }
+        }
+    }
+}
+
+static int check(int *visited, int n){
+    for(int i = 0;i < n; i++){
+        if(visited[i] != 1){
+            return 0;
+        }
+        visited[i] = 0;
+    }
+    return 1;
+}
+// YOUR SOLUTIONS BELOW
+
+int q1(int n, const connection_t connections[n][n])
+{
+    int ans = 1;
+    int *visited;
+    visited  = (int*)calloc(n,sizeof(int));
+
+    for(int i = 0;i < n; i++){
+        dfsQ1(i, visited, n, connections);
+        ans = ans & check(visited, n);
+    }
+    free(visited);
+    return ans;
+}
+
+int q2(const airport_t *src, const airport_t *dest, int n, int k,
+       const connection_t connections[n][n])
+{
+    return 0;
+}
+
+int q3(const airport_t *src, int n, const connection_t connections[n][n])
+{
+    return 0;
+}
+
+void q4(int n, int (*predicate_func)(const airport_t *, const airport_t *),
+        airport_t airport_list[n])
+{
+
+}
+
+pair_t q5(int n, airport_t airports[n])
+{
+    pair_t ans = {-1, -1};
+    return ans;
+}
+
+int q6(int n, int amount, const int entry_fee[n])
+{
+    return 0;
+}
+
+void q7(int n, const char *pat, int contains[n], const airport_t airports[n])
+{
+
+}
+
+int q8(int n, int trip_order[n - 1], const connection_t connections[n][n])
+{
+    return 0;
+}
+
+int q9(int n, pair_t edges[n - 1], const connection_t connections[n][n])
+{
+    return 0;
+}
+
+void q10(int n, int k, const airport_t *src,
+         const connection_t connections[n][n], const int destinations[k],
+         int costs[k])
+{
+
+}
+
+// END
