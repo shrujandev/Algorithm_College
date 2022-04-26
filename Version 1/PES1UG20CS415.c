4c19c9b89fe4cd2de31647d446c803e40e56d606
shrujandev <shrujandev1824@gmail.com>
2022-04-26 17:49:54 +0530

    Q2 Passed

--- Version 1/PES1UG20CS415.c
+++ Version 1/PES1UG20CS415.c
@@ -26,10 +26,36 @@ static int check(int *visited, int n){
     }
     return 1;
 }
+
+static void dfsQ2(int src, int *visited, int n, const connection_t graph[n][n], int dest, int num, int *ans){
+    visited[src] = 1;
+    
+    if(src == dest){
+        int flag = 0;
+        for(int i = 0;i < n;i++){
+            if(visited[i] == 1){
+                flag++;
+            }
+        }
+
+        if(flag <= num+1){
+            *ans = 1;
+        }
+    }
+
+    for(int i = src;i < n;i++){
+        for(int j = 0;j < n; j++){
+            if(graph[i][j].distance != __INT_MAX__ && visited[j] != 1){
+                dfsQ2(j, visited, n, graph, dest, num, ans);
+            }
+        }
+    }
+
+}
+
 // YOUR SOLUTIONS BELOW
 
-int q1(int n, const connection_t connections[n][n])
-{
+int q1(int n, const connection_t connections[n][n]){
     int ans = 1;
     int *visited;
     visited  = (int*)calloc(n,sizeof(int));
@@ -42,10 +68,21 @@ int q1(int n, const connection_t connections[n][n])
     return ans;
 }
 
-int q2(const airport_t *src, const airport_t *dest, int n, int k,
-       const connection_t connections[n][n])
-{
-    return 0;
+int q2(const airport_t *src, const airport_t *dest, int n, int k,const connection_t connections[n][n]){
+    int srcVal = src->num_id;
+    int destVal = dest->num_id;
+    int ans = 0; 
+    int *visited;
+    visited  = (int*)calloc(n,sizeof(int));
+
+    dfsQ2(srcVal, visited, n, connections, destVal, k, &ans);
+
+    free(visited);
+
+    if(ans)
+        return 1;
+    else
+        return 0;
 }
 
 int q3(const airport_t *src, int n, const connection_t connections[n][n])
